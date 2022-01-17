1fe536cb2ef469844d7c3b9f31a964123653d437
Marcio Luis Hamasaki da silva <marcio_luis@live.com>
2022-01-17 19:44:55 -0300

    [BACKEND] Added mongo and connection between backend and db.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,35 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'logs', 'access.log'),
+  { flags: 'a' }
+);
 
-app.listen(3000);
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use(bodyParser.json());
+
+mongoose.connect(
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@trac_db:27017?authSource=admin`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error('failed to connect to DB');
+      console.error(err);
+    } else {
+      console.log('connected to DB');
+      app.listen(8000);
+    }
+  }
+);
